a933f2b995fbc718a0f9f3360c920caf6962b21c
yuuyatea <yuuyatealive@gmail.com>
2020-05-16 09:57:23 +0900

    linebot削除

---
+++ view/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title></title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<link rel="stylesheet" href="css/card.css">
+</head><body>
+  <section class="card">
+    <img class="card-img" src="images/bear.jpg" alt="">
+    <div class="card-content">
+      <h1 class="card-title">Webクリエイターボックス</h1>
+      <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。</p>
+    </div>
+    <div class="card-link">
+      <a href="http://webcreatorbox.com/about">About</a>
+      <a href="http://webcreatorbox.com/">Website</a>
+    </div>
+  </section>
+
+</body>
+</html>
